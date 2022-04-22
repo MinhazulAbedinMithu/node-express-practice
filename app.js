5837a3d6f4c11f079aab143d3bb61979db781298
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2022-04-23 04:18:27 +0600

    [FEAT] : Navigate between diff routes & catchAllRoute

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-	res.send("Hello World");
+	res.status(200).send("<h1>Home Page</h1>");
+});
+
+app.get("/about", (req, res) => {
+	res.status(200).send("<h1>Home Page</h1><a href='/'>Back to home</a>");
+});
+
+app.get("*", (req, res) => {
+	res.status(404).send("Content not Found !!!");
 });
 
 //app listening :
