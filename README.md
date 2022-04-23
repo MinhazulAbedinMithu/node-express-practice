a5d2fccc80416384481eac374c5939b2a83143b9
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2022-04-23 18:23:01 +0600

    [FEAT] : basicunderstanding of  middleware

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # node-express-practice
+
+### Middleware:
+
+- Middleware are function that execute during lifecycle of a request to the server.
+- request => Middleware(do something) => response
--- app.js
+++ app.js
@@ -3,69 +3,24 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-const { users } = require("./fakeData");
+// middleware : req > do something > res
+const logger = (req, res, next) => {
+	const method = req.method;
+	const url = req.url;
+	const year = new Date().getFullYear();
 
-app.get("/", (req, res) => {
+	console.log(method, url, year);
+	next()
+}
+
+app.get("/", logger, (req, res) => {
 	res.status(200).send("<h1>Home Page</h1>");
 });
 
-app.get("/about", (req, res) => {
+app.get("/about",logger, (req, res) => {
 	res.status(200).send("<h1>Home Page</h1><a href='/'>Back to home</a>");
 });
 
-//read JSON to show users
-app.get("/users", (req, res) => {
-	const newUsers = users.map((user) => {
-		const { id, name, email, phone, website } = user;
-		return { id, name, email, phone, website };
-	});
-	res.status(200).json(newUsers);
-});
-
-//params to show single User
-app.get("/users/:userID", (req, res) => {
-	console.log(req.params.userID);
-	const singleUser = users.find(
-		(user) => user.id === Number(req.params.userID)
-	);
-	if (!singleUser) {
-		res
-			.status(404)
-			.send("<h1>Content Not Found</h1><a href='/'>Back to home</a>");
-	}
-	res.status(200).send(singleUser);
-});
-
-//Query String: Search and set limit
-app.get("/api/users/query", (req, res) => {
-	const { search, limit } = req.query;
-	let searchedUsers = [...users];
-	if (search) {
-		searchedUsers = searchedUsers.filter((user) => {
-			return user.name.startsWith(search);
-		});
-	}
-
-	if (limit) {
-		searchedUsers = searchedUsers.slice(0, Number(limit));
-	}
-
-	if (searchedUsers.length < 1) {
-		return res.status(200).json({
-			success: true,
-			data: [],
-			message: "No user matched with your search key",
-		});
-	}
-
-	res.send(searchedUsers);
-});
-
-app.get("*", (req, res) => {
-	res
-		.status(404)
-		.send("<h1>Content Not Found</h1><a href='/'>Back to home</a>");
-});
 
 //app listening :
 app.listen(port, () => {
