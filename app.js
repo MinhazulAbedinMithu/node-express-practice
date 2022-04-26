b408d8d364bf43e1e765c870475ef25432ea2002
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2022-04-27 00:38:09 +0600

    [FEAT] : Code in MVC pattern : routes & controllers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
-let { people } = require("./fakeData");
+const peopleRoutes = require("./routes/people");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 const port = 5000;
@@ -11,51 +12,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //HTTP Methods :::
+app.use("/auth", authRoutes);
+app.use("/api/people", peopleRoutes);
 
-app.get("/api/people", (req, res) => {
-	res.status(200).json({ success: true, data: people });
-});
-
-app.post("/api/people", (req, res) => {
-	const { name } = req.body;
-	if (!name) {
-		return res
-			.status(401)
-			.json({ success: false, msg: "Please Provide name value" });
-	}
-	res.status(201).json({ success: true, person: name });
-});
-
-app.put("/api/people/:id", (req, res) => {
-	const { id } = req.params;
-	const { name } = req.body;
-	const person = people.find((p) => p.id === Number(id));
-	if (!person) {
-		return res
-			.status(401)
-			.json({ success: false, msg: `Couldn't found id: ${id}` });
-	}
-
-	const newPeople = people.map((p) => {
-		if (p.id === Number(id)) {
-			p.name = name;
-		}
-		return p;
-	});
-
-	res.status(201).json({ success: true, people: newPeople });
-});
-
-app.delete("/api/people/:id", (req, res) => {
-	const person = people.find((p) => p.id === Number(req.params.id));
-	if (!person) {
-		return res
-			.status(404)
-			.json({ success: false, msg: `Couldn't found id: ${req.params.id}` });
-	}
-	const newPeople = people.filter((p) => p.id !== person.id);
-	res.status(200).json({ success: true, people: newPeople });
-});
 
 //app listening :
 app.listen(port, () => {
